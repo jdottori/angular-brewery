2db8a9157de4c3eb8344ce04ff5a253c80c407cd
Javier Dottori <javier.dottori@gmail.com>
2020-10-08 13:00:46 -0300

    Adds event binding in beerlist

--- src/app/beer-list/beer-list.component.ts
+++ src/app/beer-list/beer-list.component.ts
@@ -16,6 +16,7 @@ export class BeerListComponent implements OnInit {
       stock: 5,
       image: 'assets/img/porter.jpeg',
       clearance: false,
+      quantity: 0,
     },
     {
       name: 'Red Red Wine',
@@ -24,6 +25,7 @@ export class BeerListComponent implements OnInit {
       stock: 3,
       image: 'assets/img/porter.jpeg',
       clearance: true,
+      quantity: 0,
     },
     {
       name: 'Yellow Submarine',
@@ -32,6 +34,7 @@ export class BeerListComponent implements OnInit {
       stock: 0,
       image: 'assets/img/porter.jpeg',
       clearance: false,
+      quantity: 0,
     },
     {
       name: 'Yellow Submarine 2',
@@ -40,6 +43,7 @@ export class BeerListComponent implements OnInit {
       stock: 0,
       image: 'assets/img/porter.jpeg',
       clearance: false,
+      quantity: 0,
     },
   ];
 
@@ -49,4 +53,16 @@ export class BeerListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  upQuantity(beer: Beer): void {
+    if(beer.quantity < beer.stock)
+      beer.quantity++;
+  }
+
+  downQuantity(beer: Beer): void {
+    if(beer.quantity > 0)
+      beer.quantity--;
+  }
+  changeQuantity(event, beer: Beer): void {
+    console.log(event.key);
+  }
 }
